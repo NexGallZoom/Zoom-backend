6ba35bc6981cbc890dc59fca7d573909d20fba85
SoonHyung_Lee <tnsgud9@naver.com>
2021-03-25 22:00:09 +0900

    add ssl

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import https from 'https';
+import fs from 'fs';
 import { createServer, Server } from 'http';
 
+import * as path from 'path';
 import controller from './controller';
 import { initWebSocket } from './middleware/socket';
+import config from './config';
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, config.sslKey), 'utf-8'),
+  cert: fs.readFileSync(path.join(__dirname, config.sslCrt), 'utf-8'),
+};
 
 const app = express();
+
+const httpsServer = https.createServer(options, app);
 
 app.use(bodyParser.json());
 app.use(controller);
